fe8666b310be729e7f33b6011c2262a730a9edf1
Paweł Pokorski <pawelpokorski@interia.eu>
2025-06-20 17:40:29 +0200

    Update Program.cs

--- problems/006-sum-square-difference/csharp/Program.cs
+++ problems/006-sum-square-difference/csharp/Program.cs
@@ -1,1 +1,35 @@
+using System;
 
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(SquareOfSum(100) - SumOfSquares(100));
+    }
+    
+    static long SumOfSquares(int n)
+    {
+        long sum = 0;
+        
+        for(int i = 1; i <= n; i++)
+        {
+            sum += i * i;
+        }
+        
+        return sum;
+    }
+    
+    static long SquareOfSum(int n)
+    {
+        int sum = 0;
+        
+        for(int i = 1; i <= n; i++)
+        {
+            sum += i;
+        }
+        
+        long square = sum * sum;
+        
+        return square;
+    }
+}
